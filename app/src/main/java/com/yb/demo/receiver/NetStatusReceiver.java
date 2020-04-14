c3ed31d79a5cfaabd6f018e27679139304e86984
yuanbin <2460666162@qq.com>
2020-04-14 17:36:38 +0800

    add

---
+++ app/src/main/java/com/yb/demo/receiver/NetStatusReceiver.java
@@ -0,0 +1,38 @@
+package com.yb.demo.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+public class NetStatusReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e("test","net receive call");
+        if (!isNetworkAvailable(context)) {
+            Toast.makeText(context, "network disconnected!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 网络是否可用
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager mgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] info = mgr.getAllNetworkInfo();
+        if (info != null) {
+            for (int i = 0; i < info.length; i++) {
+                if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
